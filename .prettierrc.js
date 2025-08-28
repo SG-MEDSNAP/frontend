695a3999551125b981f2bd547225ab654a3e69fc
seunggonhyeon <hyunsg5638@gmail.com>
2025-08-28 15:27:40 +0900

    chore:개발환경 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// .prettierrc.js
+module.exports = {
+  singleQuote: true, // 큰따옴표 대신 작은따옴표 사용
+  semi: true, // 문장 끝에 세미콜론 사용
+  useTabs: false, // 탭 대신 스페이스 사용
+  tabWidth: 2, // 들여쓰기는 2칸
+  trailingComma: "all", // 배열이나 객체 마지막에 항상 쉼표 추가
+  printWidth: 80, // 한 줄의 최대 길이는 80
+  arrowParens: "always", // 화살표 함수 매개변수에 항상 괄호 사용
+};
