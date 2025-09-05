82ace9bd77a7bd32349126e6bc85ff7c6db5c26e
seunggonhyeon <hyunsg5638@gmail.com>
2025-09-05 11:38:38 +0900

    chore:nativewind 환경설정

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import './global.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Opend uddddddp</Text>
+      <Text className="font-bold">Opend uddddddp</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
