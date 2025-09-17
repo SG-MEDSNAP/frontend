654c4d1ff0b45f5bbb40822c20e1967aa88d19ba
ukongee <qkrb8019@gmail.com>
2025-09-17 13:26:24 +0900

    feat: 빌드완료

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,15 @@
-/* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-
-// Expo의 기본 설정
 const config = getDefaultConfig(__dirname);
 
-// react-native-svg-transformer 설정 적용
-const { transformer, resolver } = config;
+// SVG 트랜스포머 설정
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (e) => e !== 'svg',
+);
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+config.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer',
+);
 
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
-};
-config.resolver = {
-  ...resolver,
-  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...resolver.sourceExts, 'svg'],
-};
-
-// SVG 설정이 반영된 config를 withNativeWind로 감싸서 최종적으로 export
 module.exports = withNativeWind(config, { input: './global.css' });
