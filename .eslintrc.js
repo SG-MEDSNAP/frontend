81639e7ffa603832dc7d815591a8c1640bbd25ea
ukongee <qkrb8019@gmail.com>
2025-09-17 20:18:24 +0900

    fix: 빌드복원

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
-// .eslintrc.js
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", // TypeScript 코드를 분석하기 위한 파서
-  plugins: ["@typescript-eslint", "prettier"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier", // Prettier와 충돌하는 ESLint 규칙을 끔
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
+  settings: {
+    'import/resolver': {
+      typescript: { alwaysTryTypes: true },
+    },
+  },
   rules: {
-    "prettier/prettier": "error", // Prettier 규칙에 맞지 않으면 ESLint 오류로 표시
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 'off',
   },
 };
